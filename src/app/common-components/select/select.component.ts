1d828251a911bfaf24e8edc88ce4ec53dd907572
natasa <natasa@Desktop>
2025-10-12 11:15:50 +0200

    search

--- src/app/common-components/select/select.component.ts
+++ src/app/common-components/select/select.component.ts
@@ -1,8 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { ComboboxDTO } from '../../model/ComboboxDTO';
 import { TranslatePipe } from '../../pipes/translate.pipe';
 import { OpenDialogService } from '../../services/open-dialog.service';
 import { FindDialogComponent } from './find-dialog/find-dialog.component';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
+import { MatFormField, MatInput } from '@angular/material/input';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-select',
@@ -24,15 +28,36 @@ export class SelectComponent {
   @Output() exportValue:EventEmitter<any>=new EventEmitter<any>();
   @Output() valueInput:EventEmitter<boolean>=new EventEmitter<boolean>();
 
+  @ViewChild('searchInput') searchField!:ElementRef<HTMLInputElement>;
+  @ViewChild('selectInput') selectInput!:MatSelect;
+
+  showSearch:boolean=false;
 
   translate:TranslatePipe=new TranslatePipe();
 
   currentValue:any;
 
+  myControl = new FormControl('');
+  filteredOptions: Observable<ComboboxDTO[]>=new Observable();
+
   constructor(
     public openDialog:OpenDialogService
   ){
 
+  }
+
+   ngOnInit() {
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): ComboboxDTO[] {
+    const filterValue = value.toLowerCase();
+
+    return this.list.filter(option => option.option.toLowerCase().includes(filterValue));
   }
 
 
@@ -54,6 +79,13 @@ export class SelectComponent {
     
   }
 
+  selectAutoCompete(event:any){
+    this.currentValue=event.option.value.toString();
+    this.exportValue.emit(this.currentValue);
+    this.valueInput.emit(true);
+    this.escapeSearchField();
+  }
+
   changeValue(event:any){
    setTimeout(() => {
     let changevalue:any=event==undefined?null:event.toString();
@@ -65,8 +97,26 @@ export class SelectComponent {
    }, 1);
   }
 
+  keyDownPressed(){
+    this.myControl.setValue('');
+    setTimeout(() => {
+      this.searchField.nativeElement.focus();
+    }, 1);
+  }
+
+  escapeSearchField(){
+    this.showSearch=false;
+
+    setTimeout(() => {
+      this.selectInput._elementRef.nativeElement.click();
+      this.selectInput.close();
+    }, 1);
+  }
 
   find(){
+    if(this.showSearch){
+      this.escapeSearchField();
+    }
     this.openDialog.openDialog(FindDialogComponent,1000,this.searchApi,false)
     .then((response)=>{
       setTimeout(() => {
@@ -81,4 +131,15 @@ export class SelectComponent {
     .catch(()=>{})
   }
 
+  checkKey(event:Event){
+    let eventAny:any=event as any;
+    if(eventAny.code=='Tab'){
+
+      return;
+    }else{
+      event.stopPropagation();
+      this.showSearch=!this.showSearch;
+      this.keyDownPressed()
+    }
+  }
 }
